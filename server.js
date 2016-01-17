f2f7b32b98f9e0a3c2c853a9e6d6dae662f28d58
Nick Barry <niko.barry@gmail.com>
2016-01-17 12:46:04 -0800

    basic server file

---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Nicholas on 1/17/2016.
+ */
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    _ = require('underscore'),
+    json = require('./movies.json'),
+    app = express();
+
+app.set('port', process.ev.PORT || 3500);
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+var router = new express.Router();
+// TODO: Set up endpoints
+app.use('/', router);
+
+var server = app.listen(app.get('port'), function(){
+    console.log('Server up: http://localhost:' + app.get('port'));
+});
